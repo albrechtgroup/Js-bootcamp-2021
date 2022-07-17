6744a6fe1a55a5a3c359f9f69bd0f510e7cac1e8
Andy Albrecht <aalbrecht33733@gmail.com>
2022-07-17 15:15:24 -0400

    while loops logic and more comments

--- loops/whileLoops/app.js
+++ loops/whileLoops/app.js
@@ -1,16 +1,28 @@
+/// 'while' loop - continues to run as long as the
+/// condition is true. *Cautious of INFINITE Loops*
+/// The 'for' we have seen
 for (let i = 0; i <= 7; i++) {
 	console.log(i);
 }
-console.log('---------------------------');
+console.log('----------------------');
+
+/// 'while' loop
+let num = 0;
+while (num < 11) {
+	console.log(num);
+	num++;
+}
+console.log('----------------------');
 
 let j = 0;
 while (j <= 7) {
 	console.log(j);
 	j++;
 }
-console.log('---------------------------');
-//////////
+console.log('----------------------');
 
+/// *While loop is good for situations when you dont
+/// know how many times the loop will run..*
 const target = Math.floor(Math.random() * 10);
 let guess = Math.floor(Math.random() * 10);
 while (guess !== target) {
@@ -18,4 +30,8 @@ while (guess !== target) {
 	guess = Math.floor(Math.random() * 10);
 }
 console.log(`Target: ${target} Guess: ${guess}`);
-console.log('----------------------------');
+console.log('Congradulations, THE END')
+console.log('-----------------------');
+/// while(some condition)
+/// in the loop, update or attempt to make thet 
+/// condition false.
