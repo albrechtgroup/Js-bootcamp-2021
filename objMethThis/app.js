742234f2c3de7116416f40533611cc838949033c
Andy Albrecht <aalbrecht33733@gmail.com>
2022-09-29 13:10:41 -0400

    Invocation context for 'this'

--- objMethThis/app.js
+++ objMethThis/app.js
@@ -25,7 +25,8 @@ const math = {
 
 math.multiply(3, 7); // 21
 
-// Method Shorthand syntax:
+// Method Shorthand syntax: Dont have to use a Key: 
+// Value pair syntax
 const mathh = {
 	blah: 'Hi!',
 	add(x, y) {
@@ -36,16 +37,31 @@ const mathh = {
 	}
 }
 
-math.add(50, 60) // 110
+mathh.add(50, 60) // 110
 
-// this.
+const auth = {
+	username: 'TommyBoy',
+	logIn() {
+		console.log("YOU ARE NOW LOGGED IN")
+	},
+	logOut() {
+		console.log("GOODBYE")
+	}
+}
+
+// Keywork 'this': "A reference to the current 
+// execution's scope"
 function sayHi() {
 	console.log("Welcome to 'this'!");
 	console.log(this);
 }
-// this refers to the window (global scope
-// object in the browser)
+// 'this' above refers to the window (global scope
+// object in the browser) With a function NOT in 
+// an object*
 
+// Using 'this' in Methods:
+// 'this' Below, console.logs the 'person' object
+// that it is in. not the global window*
 const person = {
 	first: 'Cherilyn',
 	last: 'Sarkisian',
@@ -54,18 +70,25 @@ const person = {
 		const { first, last, nickName } = this;
 		return `${first} ${last} AKA ${nickName}`;
 	},
+	// 'this' in printBio func, refers to the 'person'
+	// parent/object as well
 	printBio() {
 		console.log(this);
 		const fullName = this.fullName();
 		console.log(`${fullName} is a person!!`);
 	}
 };
-// person.fullName()
+person.fullName() // 'Cherilyn Sarkisian AKA Cher'
 
-// this here, console.logs the 'person' object
-// that it is in.
+// 'this' Invocation context: The value of 'this'
+// depends on the invocation context of the function 
+// it is used in.
+// "The value of 'this' will change, depending on 
+// how the function is executed, not just where
+// you write it"
 
-// Annoy-O-Matic
+
+// Annoy-O-Matic Demo:
 const annoyer = {
 	phrases: [ 'Literally', 'cray cray', "I can't even!", 'Totes', 'YOLO', "Can't stop, won't stop" ],
 	pickPhrase() {
@@ -75,3 +98,5 @@ const annoyer = {
 	},
 	start() {}
 };
+
+annoyer.pickPhrase(); // 'YOLO'
