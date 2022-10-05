e738aa6aa581a0cf9f6b480a1e87302f59b542bc
Andy Albrecht <aalbrecht33733@gmail.com>
2022-10-05 15:52:56 -0400

    Manipulating the DOM

---
+++ theDom/app.js
@@ -0,0 +1,17 @@
+// Intro to the DOM: Document Object Model
+// Bunch of Objects that you can interact with the 
+// webpage/window via JS
+
+// 'querySelector': Newer and More commonly used* All-
+// in=one Method to select a single element.
+// Pass in a CSS Selector
+
+// finds first h1 element:
+document.querySelector('h1');
+
+// finds first element with ID of blue:
+document.querySelector('#blue');
+
+// finds first element with class of big:
+document.querySelector('.big');
+
