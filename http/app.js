b343d346d2052476534e608f88cd27837e61620f
Andy Albrecht <aalbrecht33733@gmail.com>
2022-10-27 14:59:19 -0400

    JSON, Http requests and Axios

---
+++ http/app.js
@@ -0,0 +1,14 @@
+// Making HTTP Requests:
+// A.J.A.X. - Asynchronous JavaScript and XML
+// (Mostly means JSON instead of XML. *AJAJ)
+
+// X.M.L. - Extensible Markup Language *Old*
+
+// J.S.O.N. - JavaScript Object Notation: *Most Common
+
+// Fetch APT - The Newer, Better way to make requests
+// via JS. Supports Promises!
+
+// AXIOS - A librairy for making Http Requests:
+// *Most Popular*
+
