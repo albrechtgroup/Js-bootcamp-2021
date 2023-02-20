5c04b806ed67e1b7fd94ef8667ec2d58a482bee0
Andy Albrecht <aalbrecht33733@gmail.com>
2023-02-20 14:59:09 -0500

    Comments and Code update

--- functions/averagePangrams/app.js
+++ functions/averagePangrams/app.js
@@ -1,7 +1,7 @@
-/// Write a function to find the average value in 
-/// an array of numbers
-///     avg([0, 50])   // 25
-///     avg([75, 76, 80, 95, 100])  //85.2
+// Write a function to find the average value in 
+// an array of numbers
+//     avg([0, 50])   // 25
+//     avg([75, 76, 80, 95, 100])  //85.2
 function avg(arr) {
 	let total = 0;
 	// loop over each num
@@ -16,14 +16,14 @@ function avg(arr) {
 
 avg([2, 4, 6, 8, 10]); // 6
 
-/// A Pangram is a sentence that contains every 
-/// letter of the alphabet, like: "The quick brown 
-/// fox jumps over the lazy dog"
+// A Pangram is a sentence that contains every 
+// letter of the alphabet, like: "The quick brown 
+// fox jumps over the lazy dog"
 
-/// Write a function called isPangram, which checks
-/// to see if a given sentence contains every letter
-/// of the alphabet.
-///	Make sure you ignore string casing!
+// Write a function called isPangram, which checks
+// to see if a given sentence contains every letter
+// of the alphabet.
+// Make sure you ignore string casing!
 function isPangram(sentence) {
 	let lowerCased = sentence.toLowerCase();
 	for (let char of 'abcdefghijklmnopqrstuvwxyz') {
@@ -34,18 +34,18 @@ function isPangram(sentence) {
 	return true;
 }
 
-/// Write a function getCard(), which returns a 
-/// random playing card object.
-/// Ex:
-/// 	{
+// Write a function getCard(), which returns a 
+// random playing card object.
+// Ex:
+// 	{
 //		value: 'K',
 //		suit: 'clubs'
-///	}
-/// Pick a random value from:
-/// 	2,3,4,5,6,7,8,9,10,J,K,Q,A
-/// Pick a random suit from:
+//	}
+// Pick a random value from:
+// 	2,3,4,5,6,7,8,9,10,J,K,Q,A
+// Pick a random suit from:
 // 		clubs, spades, hearts, diamonds
-/// Return Both in an object.
+// Return Both in an object.
 function pick(arr) {
 	// return random element from arr
 	const idx = Math.floor(Math.random() * arr.length);
